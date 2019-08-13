8b31943ed2030d0bcb69447955f92431f779ac71
krisdaniel <krisdaniel1452@gmail.com>
2019-08-14 00:06:08 +0300

    Setup project with bootstrap.js as plugin

---
+++ nuxt.config.js
@@ -0,0 +1,25 @@
+export default {
+    head: {
+        titleTemplate: '%s - Nuxt.js',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+
+            // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+            { hid: 'description', name: 'description', content: 'Meta description' }
+        ]
+    },
+    css: [
+        '@/assets/css/loader.less'
+    ],
+    plugins: [
+        { src: '~/plugins/bootstrap.js', mode: 'client' },
+    ],
+    modules: [
+        // Using package name
+        // '@nuxtjs/axios',
+
+        // Relative to your project srcDir
+        // '~/modules/awesome.js',
+    ],
+}
